1137789239aa2774cdd3be943dc84df1bf806173
Djacon <62030101+Djacon@users.noreply.github.com>
2023-03-14 23:57:37 +0300

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from skmini.linear_model import LogisticRegression
-
-from sklearn.datasets import load_iris
-
-X, y = load_iris(return_X_y=True)
-
-model = LogisticRegression(max_iter=1000)
-model.fit(X[20:80], y[20:80])
-print(model.score(X[:100], y[:100]))
